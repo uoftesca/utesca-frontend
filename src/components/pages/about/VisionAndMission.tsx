461db95a42c36ebdee38ce4cae3bb596888d64a7
rachelkd <rachel05deng@gmail.com>
2024-10-22 19:54:26 -0400

    Fixed Mission cards layout

--- src/components/pages/about/VisionAndMission.tsx
+++ src/components/pages/about/VisionAndMission.tsx
@@ -4,7 +4,7 @@ import MissionCard from '@/components/pages/about/MissionCard';
 
 const VisionAndMission: React.FC = () => {
     return (
-        <div className='container mx-auto text-center space-y-6'>
+        <div className='w-full text-center space-y-6'>
             <h1 className='text-2xl font-bold tracking-normal md:text-4xl text-accent'>
                 Our Vision & Mission
             </h1>
@@ -13,17 +13,15 @@ const VisionAndMission: React.FC = () => {
                 students together to give back to our local community? Over
                 time, this has evolved into our three core offerings:
             </p>
-            <div className='flex flex-col items-center space-y-6'>
-                <div className='w-full flex flex-col md:flex-row md:justify-center md:space-x-6 space-y-6 md:space-y-0'>
+            <div className='flex flex-col items-center gap-6'>
+                <div className='max-w-fit grid justify-items-center grid-cols-1 md:grid-cols-2 gap-6'>
                     <MissionCard
                         Icon={Users}
                         text='Pro-bono engineering-based consulting solutions for local organizations'
-                        className='md:w-[calc(50%-12px)]'
                     />
                     <MissionCard
                         Icon={Handshake}
                         text='Fostering connections with industry professionals'
-                        className='md:w-[calc(50%-12px)]'
                     />
                 </div>
                 <MissionCard
