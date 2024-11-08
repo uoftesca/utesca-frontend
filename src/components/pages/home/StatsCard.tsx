f03a5ca4a83586d70311e7a2587ebea956254541
rachelkd <rachel05deng@gmail.com>
2024-11-08 17:58:50 -0500

    Fixed redundant TailwindCSS declarations

--- src/components/pages/home/StatsCard.tsx
+++ src/components/pages/home/StatsCard.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent } from '@/components/ui/card';
-import NumberTicker from '@/components/ui/number-ticker'; 
+import NumberTicker from '@/components/ui/number-ticker';
 
 interface StatsCardProps {
     title?: string;
@@ -8,12 +8,20 @@ interface StatsCardProps {
     className?: string;
 }
 
-const StatsCard: React.FC<StatsCardProps> = ({ title, num, text, className }) => {
+const StatsCard: React.FC<StatsCardProps> = ({
+    title,
+    num,
+    text,
+    className,
+}) => {
     return (
         <Card className={`bg-card border-none w-full max-w-sm ${className}`}>
             <CardContent className='flex flex-col items-center p-6'>
-                <p className='text-center text-lg text-accent font-bold' > {title} </p>
-                <p className="whitespace-pre-wrap text-6xl font-medium tracking-tighter text-black dark:text-white">
+                <p className='text-center text-lg text-accent font-bold'>
+                    {' '}
+                    {title}{' '}
+                </p>
+                <p className='whitespace-pre-wrap text-6xl font-medium tracking-tighter text-black dark:text-white'>
                     <NumberTicker value={num} />
                 </p>
                 <p className='text-subtle text-center text-sm'>{text}</p>
--- src/components/pages/home/StatsCard1.tsx
+++ src/components/pages/home/StatsCard1.tsx
@@ -9,15 +9,23 @@ interface StatsCard1Props {
     className?: string;
 }
 
-const StatsCard1: React.FC<StatsCard1Props> = ({ title, num, text, text1, className }) => {
+const StatsCard1: React.FC<StatsCard1Props> = ({
+    title,
+    num,
+    text,
+    text1,
+    className,
+}) => {
     return (
         <Card className={`bg-card border-none w-full max-w-sm ${className}`}>
-            <CardContent className="flex h-full flex-col items-center justify-center p-6">
-            <p className='text-center text-lg text-sm font-bold' > {title} </p>
-                <p className='text-center text-6xl text-accent font-bold' > {num} </p>
+            <CardContent className='flex h-full flex-col items-center justify-center p-6'>
+                <p className='text-center text-sm font-bold'> {title} </p>
+                <p className='text-center text-6xl text-accent font-bold'>
+                    {' '}
+                    {num}{' '}
+                </p>
                 <p className='text-subtle text-center text-sm'>{text}</p>
                 <p className='text-subtle text-center text-sm'>{text1}</p>
-
             </CardContent>
         </Card>
     );
