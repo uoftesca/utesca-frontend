6bf55e142176cd91e00b5ca6914a6d20e68adf22
rachelkd <rachel05deng@gmail.com>
2025-01-14 21:12:25 -0500

    Added red accent to card titles

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -36,7 +36,7 @@ const CardTitle = React.forwardRef<
     <h3
         ref={ref}
         className={cn(
-            'text-2xl font-semibold leading-none tracking-tight',
+            'text-2xl font-semibold leading-none tracking-tight text-accent',
             className
         )}
         {...props}
@@ -60,7 +60,11 @@ const CardContent = React.forwardRef<
     HTMLDivElement,
     React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-    <div ref={ref} className={cn('p-6 pt-0', className)} {...props} />
+    <div
+        ref={ref}
+        className={cn('p-6 pt-0 text-subtle', className)}
+        {...props}
+    />
 ));
 CardContent.displayName = 'CardContent';
 
