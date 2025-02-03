9417792589ac76c9e4e8800e9889892b24e0b148
rachelkd <rachel05deng@gmail.com>
2025-02-03 02:12:16 -0500

    Update Next.js image configuration and package dependencies

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['plus.unsplash.com', 'images.unsplash.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'plus.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+        ],
     },
 };
 
