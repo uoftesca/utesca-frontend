5d7ce7b7c7391505981143d1e43b562dc92062a0
rachelkd <rachel05deng@gmail.com>
2025-10-10 12:57:29 -0400

    update stats

--- src/components/pages/home/Stats.tsx
+++ src/components/pages/home/Stats.tsx
@@ -2,15 +2,15 @@ import StatsCard from './StatsCard';
 
 const Stats: React.FC = () => {
   return (
-    <div className='w-full text-center space-y-6'>
-      <div className='flex flex-col items-center gap-6'>
-        <div className='max-w-fit grid justify-items-center grid-cols-1 md:grid-cols-3 gap-6'>
-          <StatsCard title='Student Executives' num={70} text='' />
-          <StatsCard title='Projects Completed' num={30} text='since 2020' />
+    <div className="w-full text-center space-y-6">
+      <div className="flex flex-col items-center gap-6">
+        <div className="max-w-fit grid justify-items-center grid-cols-1 md:grid-cols-3 gap-6">
+          <StatsCard title="Student Executives" num={80} text="" />
+          <StatsCard title="Projects Completed" num={30} text="since 2020" />
           <StatsCard
-            title='Engaged with'
+            title="Engaged with"
             num={15}
-            text='undergraduate and graduate disciplines'
+            text="undergraduate and graduate disciplines"
           />
         </div>
       </div>
