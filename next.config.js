61ffc72fb92085a96b9d3783e5b6abba8868dd2b
rachelkd <rachel05deng@gmail.com>
2025-11-12 17:36:31 -0500

    feat: add raw.githubusercontent.com to allowed image sources in next.config.js

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'drive.google.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
     ],
   },
 };
